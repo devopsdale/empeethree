cd2a51478e3b11ff37efb80d7b6533e58d3ff982
Dale Ramdeen <dramdeen1@gmail.com>
2019-04-03 11:01:16 -0400

    working to submit links and redirect back home

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/submit-form');
 
 var app = express();
 
@@ -16,5 +20,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/submit-form', usersRouter);
 
-module.exports = app;
+//input
+app.use(express.urlencoded())
+
+//get the form input
+app.post('/submit-form', (req, res) => {
+  const username = req.body.url
+  console.log(username);
+  res.redirect('/');
+  res.end()
+})
+
+module.exports = app
+
+console.log("App running: http://localhost:3000"); ;
